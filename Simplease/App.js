cc08c1fc80317fa3f11d84394fcb309ce72bc7a3
foutsm <mfouts92@gmail.com>
2017-09-24 18:54:39 -0400

    login page testing

--- Simplease/App.js
+++ Simplease/App.js
@@ -27,7 +27,7 @@ export default class BlinkApp extends Component {
   render() {
     return (
       <View>
-        <Blink text='I love to blink' />
+        <Blink text='I love to blink at you' />
         <Blink text='Yes blinking is so great' />
         <Blink text='Why did they ever take this out of HTML' />
         <Blink text='Look at me look at me look at me' />
