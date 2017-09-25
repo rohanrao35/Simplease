3c5040fdde7cbeb78388ae972e5549cb7b094053
rohanrao35 <31747010+rohanrao35@users.noreply.github.com>
2017-09-24 21:26:30 -0400

    Login screen and input

--- Simplease/App.js
+++ Simplease/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Login from './components/Login/Login'
+
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>This is a test. </Text>
+      <View>
+        <Login />
+
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
